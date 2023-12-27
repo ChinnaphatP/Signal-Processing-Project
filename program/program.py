6005e33c2bc4b758f397eff7786ade4443970f68
Chinnaphat <chinnaphat46@gmail.com>
2023-12-27 20:56:41 +0700

    Modified 1 hour to save exel.

--- program/program.py
+++ program/program.py
@@ -24,22 +24,38 @@ def get_air_quality_data():
         print("Failed to retrieve air quality data.")
         return None
 
-def save_to_excel(data):
-    current_time_thailand = datetime.utcnow() + timedelta(hours=7)
-    formatted_time = current_time_thailand.strftime("%Y-%m-%d %H:%M:%S")
-    aqius = data["aqius"]
 
-    df = pd.DataFrame({"Timestamp": [formatted_time], "AQI": [aqius]})
+def save_to_excel():
+    current_time = datetime.now()
     
+    # ตรวจสอบว่าเป็นเวลาที่ต้องการหรือไม่ (ทุกๆ 1 ชั่วโมง)
+    if current_time.minute.minute == 0 and current_time.minute.second == 0:
+        aq_data = get_air_quality_data()
+        if aq_data:
+            formatted_time = current_time.minute.strftime("%Y-%m-%d %H:%M:%S")
+            aqius = aq_data["aqius"]
+    
+            df = pd.DataFrame({"Timestamp": [formatted_time], "AQI": [aqius]})
+            
+            try:
+                with pd.ExcelWriter(EXCEL_FILE_PATH, mode="a", engine="openpyxl") as writer:
+                    df.to_excel(writer, index=False, sheet_name="AirQuality", startrow=writer.sheets["AirQuality"].max_row, header=writer.sheets["AirQuality"].max_row == 0)
+                print(f"Data saved to {EXCEL_FILE_PATH} at {formatted_time}.")
+            except Exception as e:
+                print(f"Error saving data to Excel: {e}")
+                
+                
+def run_scheduler():
+    scheduler = BlockingScheduler()
+
+    # ตั้งเวลาทำงานทุกๆ 1 ชั่วโมง
+    scheduler.add_job(save_to_excel, 'interval', hours=1, id='save_to_excel_job')
+
     try:
-        with pd.ExcelWriter(EXCEL_FILE_PATH, mode="a", engine="openpyxl") as writer:
-            df.to_excel(writer, index=False, sheet_name="AirQuality", startrow=writer.sheets["AirQuality"].max_row, header=writer.sheets["AirQuality"].max_row == 0)
-        print(f"Data saved to {EXCEL_FILE_PATH} at {formatted_time}.")
-    except Exception as e:
-        print(f"Error saving data to Excel: {e}")
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
 
 if __name__ == "__main__":
-    scheduler = BlockingScheduler()
-    scheduler.add_job(get_air_quality_data, 'interval', hours=1, id='get_air_quality_job')
-    scheduler.add_job(save_to_excel, 'interval', hours=1, args=[get_air_quality_data()], id='save_to_excel_job')
-    scheduler.start()
+    run_scheduler()
